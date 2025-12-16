c7ef30ecd701eb2f9e84a2bda87da3b555ef0933
Sebastian Döll <sebastian@katallaxie.dev>
2025-12-16 17:36:16 +0000

    feat: add github button

--- src/app/(main)/auth/v1/login/page.tsx
+++ src/app/(main)/auth/v1/login/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Command } from 'lucide-react'
 
 import { LoginForm } from '../../_components/login-form'
+import { GitHubButton } from '../../_components/social-auth/github-button'
 import { GoogleButton } from '../../_components/social-auth/google-button'
 
 export default function LoginV1() {
@@ -13,8 +14,12 @@ export default function LoginV1() {
                     <div className="space-y-6">
                         <Command className="mx-auto size-12 text-primary-foreground" />
                         <div className="space-y-2">
-                            <h1 className="font-light text-5xl text-primary-foreground">Hello again</h1>
-                            <p className="text-primary-foreground/80 text-xl">Login to continue</p>
+                            <h1 className="font-light text-5xl text-primary-foreground">
+                                Hello again
+                            </h1>
+                            <p className="text-primary-foreground/80 text-xl">
+                                Login to continue
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -25,15 +30,21 @@ export default function LoginV1() {
                     <div className="space-y-4 text-center">
                         <div className="font-medium tracking-tight">Login</div>
                         <div className="mx-auto max-w-xl text-muted-foreground">
-                            Welcome back. Enter your email and password, let&apos;s hope you remember them this time.
+                            Welcome back. Enter your email and password,
+                            let&apos;s hope you remember them this time.
                         </div>
                     </div>
                     <div className="space-y-4">
                         <LoginForm />
                         <GoogleButton className="w-full" variant="outline" />
+                        <GitHubButton className="w-full" variant="outline" />
                         <p className="text-center text-muted-foreground text-xs">
                             Don&apos;t have an account?{' '}
-                            <Link prefetch={false} href="register" className="text-primary">
+                            <Link
+                                prefetch={false}
+                                href="register"
+                                className="text-primary"
+                            >
                                 Register
                             </Link>
                         </p>
--- src/app/(main)/auth/v2/login/page.tsx
+++ src/app/(main)/auth/v2/login/page.tsx
@@ -5,6 +5,7 @@ import { Globe } from 'lucide-react'
 import { APP_CONFIG } from '@/config/app-config'
 
 import { LoginForm } from '../../_components/login-form'
+import { GitHubButton } from '../../_components/social-auth/github-button'
 import { GoogleButton } from '../../_components/social-auth/google-button'
 
 export default function LoginV2() {
@@ -21,6 +22,7 @@ export default function LoginV2() {
                 </div>
                 <div className="space-y-4">
                     <GoogleButton className="w-full" />
+                    <GitHubButton className="w-full" />
                     <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-border after:border-t">
                         <span className="relative z-10 bg-background px-2 text-muted-foreground">
                             Or continue with
