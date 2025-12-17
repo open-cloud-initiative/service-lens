28b98b0da2da2003443d8497f65cce7c0a766f7c
Sebastian Döll <sebastian@katallaxie.dev>
2025-12-17 13:07:13 +0000

    chore: update url

--- src/db/config/config.ts
+++ src/db/config/config.ts
@@ -4,43 +4,41 @@ import { Sequelize } from 'sequelize-typescript'
 
 const env = process.env.NODE_ENV || 'development'
 
-const models = [
- Design
-]
+const models = [Design]
 
 export interface Config {
-  [index: string]: SequelizeOptions
+    [index: string]: SequelizeOptions
 }
 
 export const config: Config = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialectModule: require('pg'),
-    dialect: 'postgres',
-    models
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialectModule: require('pg'),
-    dialect: 'postgres',
-    logging: false,
-    models
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectModule: require('pg'),
-    models
-  }
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialectModule: require('pg'),
+        dialect: 'postgres',
+        models,
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialectModule: require('pg'),
+        dialect: 'postgres',
+        logging: false,
+        models,
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        dialectModule: require('pg'),
+        models,
+    },
 }
 
 const connection = new Sequelize({ ...config[env] })
