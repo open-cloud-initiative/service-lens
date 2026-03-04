f3d53e8759a50e950511ffd0621de8ba7db6326c
Sebastian Döll <sebastian@katallaxie.dev>
2026-03-04 13:06:52 +0000

    chore: set base url for better auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from 'better-auth/react'
 
 export const client = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BETTER_AUTH_URL ?? 'http://localhost:3000',
     plugins: [
         organizationClient({
             teams: { enabled: true },
